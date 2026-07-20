04709ff6200d33a6dd394473085d394dc1ce75bf
Muhammad <mohammadjoyo635@gmail.com>
2026-07-20 02:43:16 +0000

    learn_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1234
+let accountEmail="muhammadjoyo540@gmail.com"
+var accountPassword="42255"
+accountCity="Karachi"
+let accountState
+// accountId=424    not allowed
+
+console.log(accountId)
+
+accountEmail="muhammad@gmaiil.com"
+accountPassword="24543"
+accountCity="dadu"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
