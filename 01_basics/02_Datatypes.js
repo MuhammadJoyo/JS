740218f2e2b27935629c4e126e695b7417ef81f4
Muhammad <mohammadjoyo635@gmail.com>
2026-07-23 02:41:23 +0000

    DataTypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as new version
+
+//alert(3+3) //we use node.js not browser
+
+console.log(3+3) //code resdability should be high
+
+console.log("Muhammad")
+
+let name="muhammad"
+let age=21
+let loggedIn= false
+
+//number =>2 to power 53
+
+//bigInt
+//string=>""
+//boolean=>true or false
+//null=>standalone vlaue
+//undefined
+//symbol=>unique
+
+
+console.log(typeof undefined); //undefind
+console.log(typeof null); //Object
